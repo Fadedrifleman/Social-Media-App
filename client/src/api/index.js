83d69d46947b566ac4426417fe09305d7235753f
Shivam Uttam <102919037+Fadedrifleman@users.noreply.github.com>
2023-03-04 11:15:01 +0530

    .

--- client/src/api/index.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-const url = "http://localhost:5000/posts";
-
-export const fetchPost = ()=> axios.get(url);
--- client/src/reducers/index.js
+++
@@ -1,4 +0,0 @@
-import { combineReducers } from "redux";
-import posts from "./posts.js"
-
-export default combineReducers({ posts });
